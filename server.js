806f7641ede159b37ac4b920470b0830b9fda692
rudra <rudra@sambuq.com>
2024-03-23 16:26:29 +0530

    elastic search

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {
+  insertAll,
+  insertOne,
+  search,
+  deleteOne,
+  updateOne,
+} = require("./test-es");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Insert all test API
+app.post("/api/test/bulk", async (req, res) => {
+  try {
+    const response = await insertAll(req.body);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Insert one solution API
+app.post("/api/test", async (req, res) => {
+  const { mongoId, data } = req.body;
+  try {
+    const response = await insertOne(mongoId, data);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Search test API
+app.get("/api/test/search", async (req, res) => {
+  const { businessId, cardtype, query } = req.query;
+  try {
+    const response = await search(businessId, cardtype, query);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Update one solution API
+app.put("/api/test/:mongoId", async (req, res) => {
+    const mongoId = req.params.mongoId;
+    const newData = req.body;
+    try {
+        const response = await updateOne(mongoId, newData);
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+// Remove one solution API
+app.delete("/api/test/:mongoId", async (req, res) => {
+  const mongoId = req.params.mongoId;
+  try {
+    const response = await deleteOne(mongoId);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ test-es.js
@@ -0,0 +1,191 @@
+const unirest = require("unirest");
+
+// insert all test
+async function insertAll(testArray) {
+  // must modify the _id to mongo_id else elasticsearch will throw an error
+  return new Promise((resolve, reject) => {
+    const bulkData = testArray.flatMap((doc) => [
+      { index: { _index: "test", _id: doc.mongo_id } },
+      doc,
+    ]);
+    unirest
+      .post("https://localhost:9200/test/_bulk")
+      .auth({
+        user: "elastic",
+        password: "1FvbJnCYX-JTSvj8XEB4",
+        sendImmediately: true,
+      })
+      .headers({
+        "Content-Type": "application/json",
+      })
+      .strictSSL(false)
+      .send(bulkData.map(JSON.stringify).join("\n") + "\n")
+      .end(function (response) {
+        if (response.error) {
+          console.error("Error:", response.error);
+          reject(response.error);
+        } else {
+          console.log("Bulk Upload Response:", response.raw_body);
+          resolve(JSON.parse(response.raw_body));
+        }
+      });
+  });
+}
+
+// insert one solution
+async function insertOne(mongoId, data) {
+  return new Promise((resolve, reject) => {
+    unirest
+      .post("https://localhost:9200/test/_doc/" + mongoId)
+      .auth({
+        user: "elastic",
+        password: "1FvbJnCYX-JTSvj8XEB4",
+        sendImmediately: true,
+      })
+      .headers({
+        "Content-Type": "application/json",
+      })
+      .strictSSL(false)
+      .send(JSON.stringify(data))
+      .end(function (response) {
+        if (response.error) {
+          console.error("Error: ", response.error);
+          reject(response.error);
+        } else {
+          console.log("Insert One Solution Response:", response.raw_body);
+          resolve(JSON.parse(response.raw_body));
+        }
+      });
+  });
+}
+
+// Search solution
+async function search(businessId, cardtype, query) {
+  return new Promise((resolve, reject) => {
+    unirest
+      .get("https://localhost:9200/test/_search")
+      .auth({
+        user: "elastic",
+        password: "1FvbJnCYX-JTSvj8XEB4",
+        // sendImmediately: true,
+      })
+      .headers({
+        "Content-Type": "application/json",
+      })
+      .strictSSL(false)
+      .send
+      // JSON.stringify({
+      //   size: 10,
+      //   sort: [
+      //     {
+      //       _score: "desc",
+      //     },
+      //   ],
+      //   query: {
+      //     bool: {
+      //       must: [
+      //         {
+      //           match: {
+      //             cardtype: cardtype,
+      //           },
+      //         },
+      //         {
+      //           match: {
+      //             business: businessId,
+      //           },
+      //         },
+      //         {
+      //           wildcard: {
+      //             solutionname: {
+      //               value: `*${query.toLowerCase()}*`,
+      //             },
+      //           },
+      //         },
+      //       ],
+      //       should: [
+      //         {
+      //           wildcard: {
+      //             solutionname: {
+      //               value: `*${query.toLowerCase()}*`,
+      //             },
+      //           },
+      //         },
+      //         {
+      //           wildcard: {
+      //             solutionname: {
+      //               value: `${query}*`,
+      //               boost: 10,
+      //             },
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   },
+      // })
+      ()
+      .end(function (response) {
+        if (response.error) {
+          console.error("Error: ", response.error);
+          reject(response.error);
+        } else {
+          console.log("Search Medicine Response:", response.raw_body);
+          resolve(JSON.parse(response.raw_body));
+        }
+      });
+  });
+}
+
+// Update one solution
+async function updateOne(mongoId, newData) {
+  return new Promise((resolve, reject) => {
+    unirest
+      .post("https://localhost:9200/test/_update/" + mongoId)
+      .auth(process.env.ES_USERNAME, process.env.ES_PASSWORD)
+      .headers({
+        "Content-Type": "application/json",
+      })
+      .strictSSL(false)
+      .send(JSON.stringify({ doc: newData }))
+      .end(function (response) {
+        if (response.error) {
+          console.error("Error: ", response.error);
+          reject(response.error);
+        } else {
+          console.log("Update One Solution Response:", response.raw_body);
+          resolve(JSON.parse(response.raw_body));
+        }
+      });
+  });
+}
+
+// Remove one solution
+async function deleteOne(mongoId) {
+  return new Promise((resolve, reject) => {
+    unirest
+      .delete("https://localhost:9200/test/_doc/" + mongoId)
+      .auth({
+        user: "elastic",
+        password: "1FvbJnCYX-JTSvj8XEB4",
+        sendImmediately: true,
+      })
+      .headers({
+        "Content-Type": "application/json",
+      })
+      .strictSSL(false)
+      .end(function (response) {
+        if (response.error) {
+          console.error("Error: ", response.error);
+          reject(response.error);
+        } else {
+          console.log("Remove One Solution Response:", response.raw_body);
+          resolve(JSON.parse(response.raw_body));
+        }
+      });
+  });
+}
+
+exports.insertAll = insertAll;
+exports.insertOne = insertOne;
+exports.search = search;
+exports.updateOne = updateOne;
+exports.deleteOne = deleteOne;
